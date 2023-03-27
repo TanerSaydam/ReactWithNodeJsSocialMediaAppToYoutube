3f04314a3ba966b06a4b5b00b009eb972b062833
tanersaydam <tanersaydam@gmail.com>
2023-03-27 21:52:08 +0300

    .

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,36 @@
 function Home(){
     return(
-        <>
-        </>
+        <div className="d-flex justify-content-center mt-4">
+            <div className="col-md-6">
+                <div className="card">
+                    <div className="card-body">
+                        <form>
+                            <div className="form-group">
+                            <textarea className="form-control" rows="5" placeholder="Ne düşünüyorsunuz...">
+                            </textarea>
+                            </div>
+                            <div className="form-group mt-2">
+                            <button className="btn btn-primary" style={{float:"right"}}>Share</button>
+                            </div>
+                        </form>
+                    </div>                    
+                </div>
+                <hr/>
+                <div className="card mt-2">
+                    <div className="card-body">
+                        <h5>Taner Saydam - 27.03.2023 21:51</h5>
+                        <p>asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasd asdasdasdasdasdasdasdasd</p>
+                    </div>
+                </div>
+
+                <div className="card mt-2">
+                    <div className="card-body">
+                        <h5>Taner Saydam - 27.03.2023 21:51</h5>
+                        <p>asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasd asdasdasdasdasdasdasdasd</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
